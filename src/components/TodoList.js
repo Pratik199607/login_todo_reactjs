692d61abcd2289537b68c3fae9e94805f31cc520
Pratik199607 <bapatp7@gmail.com>
2023-06-07 16:08:17 +0530

    Fourth Commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,89 @@
-// import axios from 'axios';
-import React from "react";
-import { CardComponent } from "./CardComponent";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import CardComponent from "./CardComponent";
 import { Button } from "flowbite-react";
 
 const TodoList = () => {
+  //State for fetching Todo
+  const [todos, setTodos] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  
+  //State for Create Todo
+  const [newTodo, setNewTodo] = useState("");
+  const userId = localStorage.getItem("userId");
 
-    return (
-        <div className="mx-auto container max-w-6xl text-center my-14 bg-slate-200 rounded-3xl flex flex-col items-center">
-            <input className="rounded-xl mt-5 max-w-screen-sm" type="text" />
-            {/* <button className="mt-5 py-2 w-36 rounded-xl bg-purple-600">Create Todo</button> */}
-            <Button
-                gradientDuoTone="greenToBlue"
-                outline
-                pill
-                className="w-40 mt-5"
-            >
-                Create Todo
-            </Button>
-            <CardComponent />
-        </div>
-    )
+  const handleCreateTodo = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/api/todo", { userId, task: newTodo });
+      const createdTodo = response.data.todo;
+      setTodos((prevTodos) => [...prevTodos, createdTodo]);
+      setNewTodo("");
+      fetchTodos(); 
+    } catch (error) {
+      console.error("Failed to create todo:", error);
+    }
+  };
+
+  const handleDeleteTodo = async (todoId) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/todo/${todoId}`);
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== todoId));
+    } catch (error) {
+      console.error("Failed to delete todo:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, [currentPage]);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/todo?userId=${userId}&page=${currentPage}`);
+      const { todos: fetchedTodos, totalPages } = response.data;
+      setTodos(fetchedTodos);
+      setTotalPages(totalPages);
+    } catch (error) {
+      console.error("Failed to fetch todos:", error);
+    }
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const renderTodos = () => {
+    return todos.map((todo) => (
+      <CardComponent
+        key={todo._id}
+        todo={todo}
+        onDelete={handleDeleteTodo}
+      />
+    ));
+  };
+
+  return (
+    <div className="mx-auto container max-w-4xl text-center my-14 bg-slate-200 rounded-xl flex flex-col items-center">
+      <input className="rounded-xl mt-5 max-w-screen-sm" type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} required />
+      <Button gradientDuoTone="greenToBlue" outline pill className="w-40 mt-5 bg-purple-600" onClick={handleCreateTodo}>
+        Create Todo
+      </Button>
+      {renderTodos()}
+      <div>
+        <button className='mt-3 mx-2' onClick={handlePrevPage} disabled={currentPage === 1}>
+          Previous Page
+        </button>
+        <button className='mt-3 mx-2' onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next Page
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoList;
